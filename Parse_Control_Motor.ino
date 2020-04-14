4386e614e0251e6ad0c0e775cbfa8c123a2542d0
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2020-04-14 22:17:45 +0700

    Update Parse_Control_Motor.ino

--- Parse_Control_Motor.ino
+++ Parse_Control_Motor.ino
@@ -35,21 +35,26 @@ void loop(){
     WAIT_DATA_UNTIL('\n');
     parse_string();
     
-    Serial.println("\n-==| DATA RECEIVED! |==-");
+    Serial.println("-==| DATA RECEIVED! |==-");
     Serial.println("Data Total\t: " + String(get_data_total()));
     Serial.println("ID motor\t\t: " + DATA_STR(0));
     Serial.println("Arah Putar\t: " + DATA_STR(1));
-    Serial.println("Nilai PWM\t: " + DATA_STR(2));
+    Serial.println("Nilai PWM\t: " + DATA_STR(2) + "\n\n");
     
-    if(DATA_STR(0) == "0" || DATA_STR(0) == "1")
-      id_m = (DATA_STR(0).toInt() + 5);
-    else id_m = 5;
+    if(DATA_STR(0) == "0"){
+      id_m = 5;
+      analogWrite(6, 0);
+    }else if(DATA_STR(0) == "1"){
+      id_m = 6;
+      analogWrite(5, 0);
+    }else id_m = 0;
     
     if(DATA_STR(1) == "0" || DATA_STR(1) == "left") pos_m = 0;
     else if(DATA_STR(1) == "1" || DATA_STR(1) == "right") pos_m = 1;
     else pos_m = 0;
     
     pwm_val = DATA_STR(2).toInt();
+    clear_str();
   }
   
   turn_motor(pos_m);
